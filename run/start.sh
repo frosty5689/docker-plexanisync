16a5a1b9cac776839aadc7c386197d0e71341eae
frosty5689 <frosty5689@gmail.com>
2020-11-16 01:07:05 -0800

    Update custom mappings every interval so no restart of container is needed

--- run/start.sh
+++ run/start.sh
@@ -6,23 +6,24 @@ fi
 echo "Updating settings.ini"
 python settingsupdater.py
 
-echo "Looking for custom mapping"
-
-if [ ! -z "${CUSTOM_MAPPINGS}" ]
-then
-  if test -f "${CUSTOM_MAPPINGS}"; then
-    echo "Found local custom mapping file and importing: ${CUSTOM_MAPPINGS}"
-    cp ${CUSTOM_MAPPINGS} /plexanisync/custom_mappings.ini
-  else
-    echo "Found remote custom mapping file and importing: ${CUSTOM_MAPPINGS}"
-    wget -O /plexanisync/custom_mappings.ini ${CUSTOM_MAPPINGS}
-  fi
-else
-  echo "No custom mapping specified"
-fi
 
 while true
 do
+
+  echo "Looking for custom mapping"
+  if [ ! -z "${CUSTOM_MAPPINGS}" ]
+  then
+    if test -f "${CUSTOM_MAPPINGS}"; then
+      echo "Found local custom mapping file and importing: ${CUSTOM_MAPPINGS}"
+      cp ${CUSTOM_MAPPINGS} /plexanisync/custom_mappings.ini
+    else
+      echo "Found remote custom mapping file and importing: ${CUSTOM_MAPPINGS}"
+      wget -O /plexanisync/custom_mappings.ini ${CUSTOM_MAPPINGS}
+    fi
+  else
+    echo "No custom mapping specified"
+  fi
+
   (cd /plexanisync && python PlexAniSync.py /config/settings.ini)
   sleep ${INTERVAL}
 done
